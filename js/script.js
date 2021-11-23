3f434ae8b6cc12410183861baca9c0b6e055e9f8
giassuer <g.assuero@hotmail.it>
2021-11-23 17:02:39 +0100

    numbers

--- js/script.js
+++ js/script.js
@@ -1,4 +1,54 @@
-let gridGame = document.getElementById("grid-game")
+// L'utente indica un livello di difficoltà in base al quale viene generata
+//  una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+// Consigli del giorno: :party_wizard:
+// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
 
-let numberFacile = [];
 
+let numberSquare = [];
+
+console.log(numberSquare);
+
+for (let i = 0; i <= numberSquare.length; i++) {
+    const number = numberSquare[i];
+
+    console.log(number);
+    
+}
+
+
+console.log(numberSquare);
+
+for (let i = 0; i <= numberSquare.length; i++) {
+    const number = numberSquare[i];
+
+    console.log(number);
+    
+}
+
+
+console.log(numberSquare);
+
+for (let i = 0; i <= numberSquare.length; i++) {
+    const number = numberSquare[i];
+
+    console.log(number);
+    
+}
+
+
+let difficoltà = 'facile';
+
+
+if (difficoltà = 'facile') {
+    numberSquare.length = 100
+    
+}else if (difficoltà = 'medio') {
+    numberSquare.length = 81
+
+}else if (difficoltà = 'difficile') {
+    numberSquare.length = 49
+}
